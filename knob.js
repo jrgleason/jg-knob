d69ca5b51a02660f0f72c9fa98ae68a9869a3611
Your Name <jackiegleason@gmail.com>
2014-09-23 21:49:47 -0400

    Fixing some stuff adding some dynamic hooks

--- knob.js
+++ knob.js
@@ -1,18 +1,44 @@
-engular.module('jg.knob', []);
+angular.module('jg.knob', []);
 
 var Directive = function(){
   var linker = function(scope, elm, attrs){
-    elm.knob({
-      displayInput : scope.displayInput,
-      cursor: scope.cursor,
-      width: scope.width,
-      min:scope.min,
-      max:scope.max,
-      step:scope.step,
-      angleOffset: scope.angleOffset,
-      linecap:scope.linecap,
-      fgColor:scope.fgColor,
-      angleArc:scope.angleArc
+    this.settings = function() {
+      return {
+        "displayInput": scope.displayInput,
+        "cursor": scope.cursor,
+        "width": scope.width,
+        "min": scope.min,
+        "max": scope.max,
+        "step": scope.step,
+        "angleOffset": scope.angleOffset,
+        "lineCap": scope.linecap,
+        "fgColor": scope.fgColor,
+        "angleArc": scope.angleArc
+      }
+    }
+    elm.knob(this.settings());
+    scope.$watch('value', function() {
+      if (typeof scope.value !== 'undefined') {
+        elm.val(scope.value).trigger('change');
+      }
+    });
+    scope.$watch('min', function() {
+      if (typeof scope.min !== 'undefined') {
+        elm.trigger(
+          'configure', {
+            "min": scope.min
+          }
+        );
+      }
+    });
+    scope.$watch('max', function() {
+      if (typeof scope.max !== 'undefined') {
+        elm.trigger(
+          'configure', {
+            "max": scope.max
+          }
+        );
+      }
     });
   }
   return {
